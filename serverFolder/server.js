79967254a0b10950105a9999330a8e32053c2035
Amanuel <weditedi25@gmail.com>
2021-05-01 14:58:35 +0100

    first route creted

---
+++ serverFolder/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (request, response) {
+  response.send("First Page");
+});
+
+//when server is ready it calls the callback function
+app.listen(3000, function () {
+  console.log("Running on port 3000 and listening");
+});
