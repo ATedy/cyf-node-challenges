50e2ed9685682ff46be8fc5f60af0c7f66abbe92
Amanuel <weditedi25@gmail.com>
2021-05-01 15:03:54 +0100

    Update server.js

--- serverFolder/server.js
+++ serverFolder/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (request, response) {
-  response.send("First Page");
+  response.send("First route");
 });
 
 //when server is ready it calls the callback function
